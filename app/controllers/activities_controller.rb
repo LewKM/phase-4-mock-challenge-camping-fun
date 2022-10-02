94c18c7222607fcded5174f7f73aa6267d057d3c
Lewis Mwendwa <lewis.mwendwa@student.moringaschool.com>
2022-10-02 20:49:56 +0300

    Adds the activity controller code.

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,4 +1,25 @@
 class ActivitiesController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
 
+    def index  
+        activities = Activity.all
+        render json: activities
+    end
+
+    def destroy
+        activity = Activity.find(params[:id])
+        activity.destroy
+        head :no_content
+    end
+
+    private
     
+    def find_activity
+        activity = Activity.find(params[:id])
+    end
+
+    def record_not_found
+        render json: { error: "Activity not found" }, status: :not_found
+    end
+
 end
